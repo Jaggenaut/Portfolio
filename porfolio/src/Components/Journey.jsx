8805be45b8545abdaa37649178265d79a6a24348
Aditya Singh <aditya.l.k.2001@gmail.com>
2023-04-04 17:38:51 +0530

    Some js in journey card to change text

--- porfolio/src/Components/Journey.jsx
+++ porfolio/src/Components/Journey.jsx
@@ -1,16 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect, useState } from 'react'
 import "./Styles/Journey.css"
 
-export class Journey extends Component {
-  render() {
-    return (
-      <div className='Journey'>
+function Journey() {
+  const [num, setnum] = useState(0)
+  let DummyData = [
+    "Lorem 1",
+    "Lorem 2",
+    "Lorem 3",
+    "Lorem 4",
+    "Lorem 5",
+    "Lorem 6",
+  ];
+  function HandleClick(){
+    if (num < (DummyData.length - 1)){
+      setnum(num+1)
+    }
+    else {
+      setnum(0)
+    }
+  }
+  function TextField(props) {
+    console.log(props[0])
+    return <div className='Journeytext'>{props.data[num]}</div>
+  }
+  useEffect(() =>{
+    setnum(num+1);
+  },1000)
+  return (
+    <div className='Journey'>
         <div className='Topic'>Journey</div>
         <div className='JourneyDescription'>
-          <div className='Journeytext'>Laqwertyuiopasdfghjklzxcvbnm,q
-wertyuioasdfghjklzxcvbnm,qwertyui
-qwertyuiopasdfghjklzxcvbnm</div>
+          <TextField data={DummyData}/>
         </div>
+        <div className='Button' onClick={HandleClick}>Next</div>
         <div className='wave'>
           <div className='waves' id='wave1'></div>
           <div className='waves' id='wave2'></div>
@@ -19,8 +41,7 @@ qwertyuiopasdfghjklzxcvbnm</div>
           <div className='waves' id='wave4'></div>
         </div>
       </div>
-    )
-  }
+  )
 }
 
 export default Journey
